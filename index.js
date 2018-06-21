11851391543ac66d439f95c84ed23ea23fece854
sophiemaniscalco <sophmaniscalco@gmail.com>
2018-06-21 20:14:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file.
+let studentsNames = ['Sophie', 'Abby', 'Mia', 'Scarlett', 'Sophia']; 
+
+function other(students) {
+  return students.toLowerCase()
+}
+
+function lowerCaseStudentNames(names) {
+  return names.map(other)
+}
+
+const fullNames: 
+
+function nameToAttributes(organize) {
+  return organize.map(function (name))
+  const firstName
+  //split name here
+  const lastName //split name here
+  return {firstName: }
+}
